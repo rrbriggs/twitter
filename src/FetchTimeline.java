ed9edc7f384a28acbe91a8ea84ff2ecfab994ad6
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-07-09 14:20:54 -0500

    refactor FetchTimeLine, move functionality of getTimeLine() to constructor

--- src/FetchTimeline.java
+++ src/FetchTimeline.java
@@ -1,23 +1,22 @@
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FetchTimeline {
     public FetchTimeline(Twitter twitter){
-        System.out.println(getTimeLine(twitter));
-    }
+        List<String> timeLine;
 
-    private static List<String> getTimeLine(Twitter twitter) {
         try {
-            return twitter.getHomeTimeline().stream()
+            timeLine = twitter.getHomeTimeline().stream()
                     .map(item -> item.getText())
                     .collect(Collectors.toList());
+            System.out.println(timeLine);
         }
         catch (TwitterException e) {
             e.printStackTrace();
         }
-        return null;
     }
 }
