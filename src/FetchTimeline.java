7a5301d9772221ca5b1b2064d2b17d439091550b
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-07-12 10:22:29 -0500

    Revert "remove app files from old location"
    This reverts commit c6181438336b242d72332d62e4aed5c2448a51cc.

---
+++ src/FetchTimeline.java
@@ -0,0 +1,21 @@
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FetchTimeline {
+    public FetchTimeline(Twitter twitter){
+        List<String> timeLine;
+
+        try {
+            timeLine = twitter.getHomeTimeline().stream()
+                    .map(item -> item.getText())
+                    .collect(Collectors.toList());
+            System.out.println(timeLine + "\n");
+        }
+        catch (TwitterException e) {
+            e.printStackTrace();
+        }
+    }
+}
