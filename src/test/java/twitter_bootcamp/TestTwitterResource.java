b211f10d7d982696e3584470bd79e20429201064
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-07-22 21:50:52 -0500

    Revert "update mock to be less redundant"
    This reverts commit c32f84145166c617cb2aac13e651b03ccdcbaa6d.

--- src/test/java/twitter_bootcamp/TestTwitterResource.java
+++ src/test/java/twitter_bootcamp/TestTwitterResource.java
@@ -6,17 +6,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import twitter4j.ResponseList;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import twitter4j.TwitterResponse;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 
 
 public class TestTwitterResource {
@@ -26,40 +22,28 @@ public class TestTwitterResource {
     @Mock
     ResponseList twitterList;
 
-    @Mock
-    TwitterResponse twitterResponse;
-
-    @Mock
-    GetTwitterInstance getTwitterInstance;
-
-    @Mock
-    Twitter twitter;
-
     @BeforeEach
     void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         twitterResource = new TwitterResource() {
             @Override
-            public Twitter getTwitter() {
-                return twitter;
+            public TwitterResponse getTwitterResponse() {
+                return twitterList;
             }
         };
     }
 
     @Test
-    final void testGetTwitterTimelineStatus() throws TwitterException {
-        when(getTwitterInstance.getTwitterTimeline()).thenReturn(twitterResponse);
+    final void testGetTwitterTimelineStatus() {
         Response response = twitterResource.getTimeline();
 
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
     }
 
     @Test
-    final void testGetTwitterTimeline() throws TwitterException {
-        when(getTwitterInstance.getTwitterTimeline()).thenReturn(twitterResponse);
-
+    final void testGetTwitterTimeline() {
         Response response = twitterResource.getTimeline();
-        System.out.println(response);
+        System.out.println(response.getEntity());
 
         assertNotNull(response.getEntity());
     }
