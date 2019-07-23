911e974e73d98714d68581f1268fa76d13767af0
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-07-23 09:36:58 -0500

    clean up unused imports and old comments

--- src/test/java/twitter_bootcamp/TestTwitterResource.java
+++ src/test/java/twitter_bootcamp/TestTwitterResource.java
@@ -3,26 +3,24 @@ package twitter_bootcamp;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import twitter4j.ResponseList;
+import twitter4j.TwitterResponse;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
-import twitter4j.*;
 
 import javax.ws.rs.core.Response;
-import java.util.LinkedList;
-import java.util.List;
 
 
 public class TestTwitterResource {
 
     TwitterResource twitterResource;
 
-    @Mock ResponseList twitterList;
+    @Mock
+    ResponseList twitterList;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -35,12 +33,8 @@ public class TestTwitterResource {
         };
     }
 
-    // TODO: This currently isn't mocking the timeline
     @Test
-    final void testGetTwitterTimelineStatus() throws TwitterException {
-
-        // when(twitterInstance.getTwitter().getHomeTimeline()).thenReturn(twitterList);
-
+    final void testGetTwitterTimelineStatus() {
         Response response = twitterResource.getTimeline();
 
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
@@ -48,11 +42,8 @@ public class TestTwitterResource {
 
     @Test
     final void testGetTwitterTimeline() {
-        // when(twitterInstance.getTwitter().getHomeTimeline()).thenReturn(twitterList);
         Response response = twitterResource.getTimeline();
-        System.out.println(response.getEntity());
-        // when(response.getHomeTimeline()).thenReturn(twitterList);
-        // response.getHomeTimeline();
+        System.out.println(response);
         assertNotNull(response.getEntity());
     }
 }
