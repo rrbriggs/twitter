e968a0f1ce868d23adc342a0835adba2244354f5
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-07-26 10:13:02 -0500

    revert GetTwitterInstance

--- src/main/java/twitter_bootcamp/config/GetTwitterInstance.java
+++ src/main/java/twitter_bootcamp/config/GetTwitterInstance.java
@@ -4,24 +4,45 @@ import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class GetTwitterInstance {
     private Twitter twitter;
 
-    public GetTwitterInstance(Properties properties) {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey(properties.getProperty("CONSUMER_KEY"))
-                .setOAuthConsumerSecret(properties.getProperty("CONSUMER_SECRET_KEY"))
-                .setOAuthAccessToken(properties.getProperty("ACCESS_TOKEN"))
-                .setOAuthAccessTokenSecret(properties.getProperty("ACCESS_TOKEN_SECRET"));
+    public GetTwitterInstance() {
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = new FileInputStream("Config.properties");
+            properties.load(inputStream);
 
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
+            // populate tokens and keys from properties file
+            String CONSUMER_KEY = properties.getProperty("CONSUMER_KEY");
+            String CONSUMER_SECRET_KEY = properties.getProperty("CONSUMER_SECRET_KEY");
+            String ACCESS_TOKEN = properties.getProperty("ACCESS_TOKEN");
+            String ACCESS_TOKEN_SECRET = properties.getProperty("ACCESS_TOKEN_SECRET");
 
-        this.twitter = twitter;
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey(CONSUMER_KEY)
+                    .setOAuthConsumerSecret(CONSUMER_SECRET_KEY)
+                    .setOAuthAccessToken(ACCESS_TOKEN)
+                    .setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
+
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter = tf.getInstance();
+
+            inputStream.close();
+
+            this.twitter = twitter;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Config.properties not found");
+        }
+
     }
 
     public Twitter getTwitter() {
