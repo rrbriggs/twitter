474240b2de86bb3053e48e9361e8fcd2f4d6f366
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-07-25 19:06:00 -0500

    rename

--- src/test/java/twitter_bootcamp/resources/TwitterResourceTest.java
+++ src/test/java/twitter_bootcamp/resources/TwitterResourceTest.java
@@ -9,7 +9,6 @@ import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter_bootcamp.resources.TwitterResource;
 
 import javax.ws.rs.core.Response;
 
@@ -18,8 +17,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 
-
-public class TestTwitterResource {
+public class TwitterResourceTest {
 
     TwitterResource twitterResource;
 
