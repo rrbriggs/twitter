38ed45a0bf26ed65dab34715653bef5d16a233e9
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-07-29 12:15:02 -0500

    add TwitterAuth class to bind twitter keys and tokens

---
+++ src/main/java/twitter_bootcamp/config/TwitterAuth.java
@@ -0,0 +1,25 @@
+package twitter_bootcamp.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class TwitterAuth {
+    @NotEmpty
+    private String consumerKey;
+    @NotEmpty private String consumerSecretKey;
+    @NotEmpty private String accessToken;
+    @NotEmpty private String accessTokenSecret;
+
+    public TwitterAuth() {}
+
+    public void setConsumerKey(String consumerKey) { this.consumerKey = consumerKey; }
+    public void setConsumerSecretKey(String consumerSecretKey) { this.consumerSecretKey = consumerSecretKey; }
+    public void setAccessToken(String accessToken) { this.accessToken = accessToken; }
+    public void setAccessTokenSecret(String accessTokenSecret) { this.accessTokenSecret = accessTokenSecret; }
+
+    @JsonProperty public String getConsumerKey() { return consumerKey; }
+    @JsonProperty public String getConsumerSecretKey() { return consumerSecretKey; }
+    @JsonProperty public String getAccessToken() { return accessToken; }
+    @JsonProperty public String getAccessTokenSecret() { return accessTokenSecret; }
+
+}
