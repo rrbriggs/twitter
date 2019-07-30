7ae4e37c0f3791b4ac2a75f54bb52a2adcc88f77
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-07-30 17:50:51 -0500

    add TwitterUser pojo

---
+++ src/main/java/twitter_bootcamp/models/TwitterUser.java
@@ -0,0 +1,51 @@
+package twitter_bootcamp.models;
+
+import java.util.Date;
+
+public class TwitterUser {
+    private String message;
+    private String twitterHandle;
+    private String name;
+    private String profileImageUrl;
+    private Date createdAt;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTwitterHandle() {
+        return twitterHandle;
+    }
+
+    public void setTwitterHandle(String twitterHandle) {
+        this.twitterHandle = twitterHandle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
