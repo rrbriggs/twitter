9cf5c690ff91cd98020f7af4ac3fc9ede61e588f
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-08-12 10:30:25 -0500

    add new object for cache update build.gradle

---
+++ src/main/java/twitter_bootcamp/services/SocialPostListCache.java
@@ -0,0 +1,21 @@
+package twitter_bootcamp.services;
+
+import java.util.List;
+import twitter_bootcamp.models.SocialPost;
+
+
+public class SocialPostListCache {
+    private List<SocialPost> socialPosts;
+
+    public SocialPostListCache(List<SocialPost> socialPosts) {
+        this.socialPosts = socialPosts;
+    }
+
+    public List<SocialPost> getSocialPosts() {
+        return socialPosts;
+    }
+
+    public void setSocialPosts(List<SocialPost> socialPosts) {
+        this.socialPosts = socialPosts;
+    }
+}
